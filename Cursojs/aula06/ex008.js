44cf289b34c2b5700593d66bbdeb0c03ce95302e
Tiasma01 <55673958+matismurf@users.noreply.github.com>
2022-11-08 10:09:51 -0300

    Add files via upload
    08/11

---
+++ Cursojs/aula06/ex008.js
@@ -0,0 +1,17 @@
+//console.log('O console funcionou corretamente!')
+/*
+var vel = 60.5
+console.log('A velocidade do seu carro é '+vel+'km/h')
+if(vel>60){
+    console.log('Você ultrapassou a velocidade permitida, MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
+*/
+
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if(pais=='Brasil'){
+    console.log('Você é Brasileiro!')
+}else{
+    console.log('Você é Estrangeiro')
+}
