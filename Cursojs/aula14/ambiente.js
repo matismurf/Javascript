04186ae12616abe6c433243b4803a8c9524585fc
Tiasma01 <55673958+matismurf@users.noreply.github.com>
2022-11-09 09:57:53 -0300

    Add files via upload

---
+++ Cursojs/aula14/ambiente.js
@@ -0,0 +1,24 @@
+console.log(`Vai começar...`)
+for(var c = 1;c<=5;c++){
+    console.log(c)
+}
+console.log(`FIM`)
+
+
+
+
+//var c = 1
+/*
+while(c<=5){
+    console.log(c)
+    c++
+}
+*/
+
+
+/*
+do{
+    console.log(c)
+    c++
+}while(c<=5)
+*/
