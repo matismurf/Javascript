a96772241e58a045f1b9a0e188606f06f33733bf
Tiasma01 <55673958+matismurf@users.noreply.github.com>
2022-11-14 19:35:56 -0300

    Add files via upload

---
+++ Cursojs/horaDoDia/exer/script.js
@@ -0,0 +1,25 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    //var hora = 20
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if(hora >= 0 && hora<12){
+        //Bom Dia!
+        img.src = 'fotomanha.png'
+        img.alt='foto da manhã'
+        document.body.style.background = '#e2cd9f'
+    }else if(hora >= 12 && hora<18){
+        //Boa Tarde!
+        img.src = 'fototarde.png'
+        img.alt = 'foto da tarde'
+        document.body.style.background = '#b9846f'
+    }else {
+        //Boa Noite!
+        img.src = 'fotonoite.png'
+        img.alt= 'foto da noite'
+        document.body.style.background = '#515154'
+    }
+}
+
